acedf65e24996bf2096d933e6295e0b9ac4df995
Alberto <albertoPcatalan@gmail.com>
2020-08-03 12:47:37 +0200

    Development in progress, solved css problem

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ const path = require('path');
 // Settings
 app.set('port', 4000); // Variable def in server for global access
 app.set('views', path.join(__dirname, 'views'));
+app.engine('html', require('ejs').renderFile); // We work with html files but we render them in ejs
 app.set('view engine', 'ejs');
 
 // Middlewares
@@ -15,6 +16,7 @@ app.set('view engine', 'ejs');
 app.use(require('./routes/'));
 
 // Static files
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 // Listening the server
--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,11 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
     // res.sendFile(path.join(__dirname + '/views/index.html'));
-    res.render('index',{title: 'First Website'}); // We give title to the view index
+    res.render('index.html',{title: 'First Website'}); // We give title to the view index
+});
+
+router.get('/contact', (req, res) => {
+    res.render('contact.html', {title: 'Contact Webpage'});
 });
 
 module.exports = router;
